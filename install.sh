b187990ae2bf2d5ba71cf22b87ffb6a6e55d51a9
louisxponce <louis.x.ponce.gmail.com>
2025-05-27 22:51:03 +0200

    Adjusted install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-ln -sf "$PWD/zshrc" ~/.zshrc
-ln -sf "$PWD/bashrc" ~/.bashrc
-ln -sf "$PWD/inputrc" ~/.inputrc
-ln -sf "$PWD/tmux.conf" ~/.tmux.conf
+# Shell configs
+ln -sf "$PWD/zshrc" $HOME/.zshrc
+ln -sf "$PWD/bashrc" $HOME/.bashrc
+ln -sf "$PWD/inputrc" $HOME/.inputrc
+ln -sf "$PWD/tmux.conf" $HOME/.tmux.conf
+
+# App configs
+ln -sf "$PWD/config/nvim" $HOME/.config/nvim
+ln -sf "$PWD/config/kitty" $HOME/.config/kitty
+
 echo "Symlinks created!"
+
