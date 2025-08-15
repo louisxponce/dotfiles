eac8a8bb022184bc252381d74c041fff213dad86
louisxponce <louis.x.ponce@gmail.com>
2025-08-15 17:18:27 +0200

    Adjusted install.sh and added zed config

--- install.sh
+++ install.sh
@@ -8,8 +8,14 @@ ln -sf "$PWD/inputrc" $HOME/.inputrc
 ln -sf "$PWD/tmux.conf" $HOME/.tmux.conf
 
 # App configs
-ln -sf "$PWD/config/nvim" $HOME/.config/nvim
-ln -sf "$PWD/config/kitty" $HOME/.config/kitty
+rm -rf "$HOME/.config/nvim"
+ln -s "$PWD/config/nvim" "$HOME/.config/nvim"
+
+rm -rf "$HOME/.config/kitty"
+ln -s "$PWD/config/kitty" "$HOME/.config/kitty"
+
+rm -rf "$HOME/.config/zed"
+ln -s "$PWD/config/zed" "$HOME/.config/zed"
 
 echo "Symlinks created!"
 
